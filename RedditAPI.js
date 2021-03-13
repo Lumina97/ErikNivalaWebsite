361d96f2a5c67678391fa84d5248777bdef6dc0e
Erik <erik04.nivala22@gmail.com>
2021-03-13 09:22:09 -0600

    Fixed reddit Authorization POST Request

--- RedditAPI.js
+++ RedditAPI.js
@@ -1,13 +1,11 @@
 const https = require('https')
 var querystring = require('querystring');
 
-
 const data = querystring.stringify({
   grant_type: 'authorization_code',
-  code: "tPoaQg7fDi2U1fqNzR7tV1YfwvPaZA#_",
-  redirect_uri: '<https://www.eriknivala.com>'
+  code: "uPO4ttTrkGqob7Mc95sqsuN0q1R-DA",
+  redirect_uri: 'https://www.eriknivala.com'
 })
-
 
 const secretstr = 'W0bNZeU8oYaUxCzqcWuf89JLMnqMVg'
 
@@ -16,25 +14,21 @@ const options = {
   path: '/api/v1/access_token',
   method: 'POST',
   headers: {
-    "Authorization": "Basic " + Buffer.from('ZHmLbNUoaVSVdw'+ ":" + secretstr, "utf8").toString("base64"),
+    "Authorization": "Basic " + Buffer.from('ZHmLbNUoaVSVdw' + ":" + "W0bNZeU8oYaUxCzqcWuf89JLMnqMVg", "utf8").toString("base64"),
     'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Content-Length': data.length
   }
 }
 
-
 const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
 
+  console.log(`statusCode: ${ res.statusCode }`)
   res.on('data', d => {
     console.log(d.toString('utf8'));
+
   })
 })
-
-req.on('error', error => {
-  console.error(error)
-})
-
+req.on('error', error => { console.error(error) })
 req.write(data)
 req.end()
