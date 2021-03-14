824188b6a8ac36b2c4ab89697ca9dc44a0793d5e
Erik <erik04.nivala22@gmail.com>
2021-03-13 23:35:47 -0500

    Fixed Commit

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const Datastore = require('nedb');
 const express = require('express');
+const RedditImageLoader = require('./RedditAPI');
+const { request, response } = require('express');
 
 const app = express();
 const database = new Datastore('database.db');
 database.loadDatabase();
 
+// RedditImageLoader.DownloadImagesFromSubreddit('gonewild',50);
+
 app.listen(3000, () => console.log("Listening at 3000"));
 
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }));
+
+
+app.post('/ImageLoader',(request, response) =>{
+    console.log("Received get request: BODY: " +request.body);
+    
+    const data = request.body;
+    RedditImageLoader.DownloadImagesFromSubreddit(data.subreddit,data.amount);
+    response.json(data);
+})
 
 
 app.post('/api', (request, response) => {
